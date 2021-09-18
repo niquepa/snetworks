0e70c03e8472765be7e94448ba5c5ceb1f469530
Carlos Castillo <ccastillo.consultant@dot.nyc.gov>
2021-09-17 21:03:54 -0400

    Rspec mocked data

---
+++ spec/requests/mocked_data.rb
@@ -0,0 +1,35 @@
+module MockedData
+  NETWORKS = {
+    "twitter": [
+      {
+        "username": "@GuyEndoreKaiser",
+        "tweet": "If you live to be 100, you should make up some fake reason why, just to mess with people... like claim you ate a pinecone every single day."
+      },
+      {
+        "username": "@mikeleffingwell",
+        "tweet": "STOP TELLING ME YOUR NEWBORN'S WEIGHT AND LENGTH I DON'T KNOW WHAT TO DO WITH THAT INFORMATION."
+      }
+    ],
+    "facebook": [
+      {
+        "name": "Some Friend",
+        "status": "Here's some photos of my holiday. Look how much more fun I'm having than you are!"
+      },
+      {
+        "name": "Drama Pig",
+        "status": "I am in a hospital. I will not tell you anything about why I am here."
+      }
+    ],
+    "instagram": [
+      {
+        "username": "hipster1",
+        "picture": "food"
+      },
+      {
+        "username": "hipster2",
+        "picture": "coffee"
+      }
+    ]
+  }
+
+end
--- spec/requests/networks_spec.rb
+++ spec/requests/networks_spec.rb
@@ -1,12 +1,15 @@
 require 'rails_helper'
+require_relative 'mocked_data'
 
 RSpec.describe "Networks", type: :request do
   describe "GET /" do
 
     it "returns 200" do
+      allow_any_instance_of(NetworksReaderService).to receive(:call) { MockedData::NETWORKS }
       get "/"
       expect(response).to have_http_status(:success)
       expect(response).to match_response_schema("networks")
     end
+    
   end
 end
